170dcb8cc92f8338edbc190a915c74a4045f5a99
MiguelRJ <miguel.rj96@gmail.com>
2017-12-13 12:44:22 +0100

    login

--- biblioteca.php
+++ biblioteca.php
@@ -12,7 +12,6 @@ function show_head($titulo){
             <link href='/sgemp/bootstrap/css/bootstrap.min.css' rel='stylesheet' media='screen'>
         </head>
         <body>
-        <h1>".$titulo."</h1>
         <script src='http://code.jquery.com/jquery.js'></script>
         <script src='/sgemp/bootstrap/js/bootstrap.min.js'></script>";
 }
--- ejercicios/login/login.php
+++ ejercicios/login/login.php
@@ -4,14 +4,23 @@ show_head("Inicio de sesion");
 ?>
 
 <div class="container">
-    <form method="POTS" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="">
-        <label for="inputUser" class="">Usuario</label>
-        <input type="text" name="user" id="inputUser" value="" required="true" autofocus="autofocus" class=""/>
-        <label for="inputPass" class="">Usuario</label>
-        <input type="password" name="password" id="inputPassword" value="" required="true" class=""/>
-        <button type="submit" class="btn btn-primary">Inicia sesion</button>
-    </form>
-</div>
+    <div class="row">
+        <form method="POTS" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="col-12 col-md-4 offset-md-4">
+        <h1 class="text-center">Inicio sesion</h1>
+            <div class="form-group">
+                <label for="inputUser" class="col-form-label">Usuario</label>
+                <input type="text" name="user" id="inputUser" value="" required="true" autofocus="autofocus" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label for="inputPass" class="col-form-label">Contraseña</label>
+                <input type="password" name="password" id="inputPassword" value="" required="true" class="form-control"/>
+            </div>
+            <div class="form-group text-right">
+                <button type="submit" class="btn btn-primary btn-align-center">Inicia sesion</button>
+            </div>
+        </form>
+    </div>
+</div> <!-- container -->
 
 <?php
 show_footer();
