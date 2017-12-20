f6d388f400e927a583d68c0fcc0d834f31967ca3
MiguelRJ <miguel.rj96@gmail.com>
2017-12-20 12:52:39 +0100

    table

--- inventory.php
+++ inventory.php
@@ -6,18 +6,35 @@ $result = $app->getDao()->getProducts();
 
 App::show_head("Inventory");
 
+echo '<div class="container"><table class="table table-hover table-dark">
+<thead>
+  <tr>
+    <th scope="col">ID</th>
+    <th scope="col">First Name</th>
+    <th scope="col">Last Name</th>
+    <th scope="col">Username</th>
+  </tr>
+</thead>
+<tbody>';
+
 if ($result->rowCount() > 0) {
     // output data of each row
     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        echo "<p>id: ".$row["id"];
-        echo " name: ".$row["name"];
-        echo " precio: ".$row["price"]."<br>";
-        echo " description: ".$row["description"]."</p>";
-        
+        echo '<tr>
+        <th scope="row">'.$row["id"].'</th>
+        <td>'.$row["name"].'</td>
+        <td>'.$row["price"].'</td>
+        <td>'.$row["description"].'</td>
+      </tr>';
+
     }
 } else {
     echo "0 results";
 }
+
+echo '</tbody>
+</table>
+</div>';
 
 App::show_footer();
 ?>
